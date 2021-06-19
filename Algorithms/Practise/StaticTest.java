966568f86f229f54ed48ce6f8167355fb95177f0
neardybhaskar <neardybhaskar@gmail.com>
2021-06-20 00:09:35 +0530

    New Codes

--- Algorithms/Practise/StaticTest.java
+++ Algorithms/Practise/StaticTest.java
@@ -9,13 +9,33 @@ public class StaticTest {
 
     private static int a;
 
+    static {
+        try {
+            a = test();
+        } catch(Exception e) {
+            e.getStackTrace();
+        }
+    }
+
+
     public static void testA() {
+        int b = 10;
         System.out.println("Do nothing");
         (new SplitChar()).test();
     }
 
+    public static void autobixingTest(Integer integer) {
+        System.out.println(integer);
+    }
+
+    public static int test() {
+        return 1;
+    }
+
     public static void main(String[] args) {
         testA();
+        int b = 5;
+        autobixingTest(b);
     }
 
 }
